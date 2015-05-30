9d2b7f79a3bc040dce27b3665a09cca6d49bce2a
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2015-05-30 09:32:00 +0200

    Added labels to bar charts.

--- helper.js
+++ helper.js
@@ -8,6 +8,7 @@
   
   var _attributes = {},
       _scales     = {},
+      _labels     = {},
       _data       = undefined,
       _d3         = undefined;
   
@@ -20,22 +21,6 @@
       .attr('class', 'barchart')
       .attr(_attributes);
     
-    var xScale = (function() {
-      if (has(_scales, 'x')) {
-        return d3.scale.linear().domain([_scales.x.minimum, _scales.x.maximum]).range([0, _attributes.width]);
-      } else {
-        return undefined;
-      }
-    })();
-    
-    var yScale = (function() {
-      if (has(_scales, 'y')) {
-        return d3.scale.linear().domain([_scales.y.minimum, _scales.y.maximum]).range([0, _attributes.height]);
-      } else {
-        return undefined;
-      }
-    })();
-    
     _d3.append('g')
       .selectAll('rect')
       .data(_data).enter()
@@ -43,10 +28,23 @@
         .attr('class', 'bar')
         .attr({
           x     : function adjustX(d, i) { return ((_attributes.width / _data.length) + 1) * i; },
-          y     : function adjustY(d, i) { return _attributes.height - ((yScale) ? yScale(d.value) : d.value); },
+          y     : function adjustY(d, i) { return _attributes.height - ((_scales.y) ? _scales.y(d.value) : d.value); },
           width : function adjustW(d, i) { return _attributes.width / _data.length; },
-          height: function adjustH(d, i) { return (yScale) ? yScale(d.value) : d.value; }
+          height: function adjustH(d, i) { return (_scales.y) ? _scales.y(d.value) : d.value; }
         });
+    
+    if (_attributes.label) {
+      _d3.append('g')
+        .selectAll('text')
+        .data(_data).enter()
+          .append('text')
+          .text(function setLabelText(d) { return d.key; })
+          .attr('text-anchor', 'middle')
+          .attr({
+            x: function adjustX(d, i) { return ((_attributes.width / _data.length) + 1 ) * i + ((_attributes.width / _data.length) / 2); },
+            y: function adjustY(d, i) { return _attributes.height - ((_scales.y) ? _scales.y(d.value) : d.value); }
+          });
+    }
     
     return this;
   };
@@ -81,18 +79,27 @@
   };
   
   helper.scale = function scale(Axis, Minimum, Maximum) {
-    var domain = {};
-    domain.minimum = Minimum;
-    domain.maximum = Maximum;
-    
     if (Axis == 'X' || Axis == 'x') {
-      _scales.x = domain;
+      _scales.x = (function() {
+        return d3.scale.linear()
+          .domain([Minimum, Maximum])
+          .range([0, _attributes.width]);
+      })();
     } else if (Axis == 'Y' || Axis == 'y') {
-      _scales.y = domain;
+      _scales.y = (function() {
+        return d3.scale.linear()
+          .domain([Minimum, Maximum])
+          .range([0, _attributes.height]);
+      })();
     } else {
-      throw "Undefined axis.";
+      throw 'Undefined axis.';
     }
     
+    return this;
+  };
+  
+  helper.label = function label() {
+    _attributes.label = true;
     return this;
   };
   
@@ -109,39 +116,6 @@
   
   helper.d3 = function getD3() {
     return _d3;
-  };
-  
-  helper.barChart = function barChart(Parameters) {
-    if (!Parameters) throw 'Missing parameters.';
-    if (!Parameters.data) throw 'Missing data.';
-    
-    var target  = Parameters.target || 'body',
-        width   = Parameters.width  ||  640,
-        height  = Parameters.height ||  480,
-        data    = Parameters.data;
-    
-    var svg = d3.select(target).append('svg')
-      .attr({
-        class   : 'barchart',
-        width   : width,
-        height  : height
-      });
-    
-    var yScale = d3.scale.linear().domain([0, d3.max(data)]).range([0, height]);
-    
-    svg.append('g')
-      .selectAll('rect')
-      .data(data).enter()
-        .append('rect')
-        .attr({
-          class : 'bar',
-          x     : function adjustX(d, i) { return (width / data.length) * i; },
-          y     : function adjustY(d, i) { return height - yScale(d); },
-          width : function adjustWidth(d, i) { return (width / data.length) - 2; },
-          height: function adjustHeight(d, i) { return yScale(d); }
-        });
-    
-    return this;
   };
   
   window.helper = helper;
